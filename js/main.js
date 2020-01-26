4d475958c0701b93264cb57e84fb76ab134f88f0
Sveta <svet.crow@gmail.com>
2020-01-26 13:12:33 +0000

    chnged styling of reviews and added menus to different languages

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ $(document).ready(function(){
 
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 200) {
             $('.back-to-top').fadeIn();
         } else {
             $('.back-to-top').fadeOut();
